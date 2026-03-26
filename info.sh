69447cfd1d3c09f64f45f8ae01954113ef53d06d
gymhub101 <johnbagger288@gmail.com>
2026-03-26 22:27:49 +0100

    Change CPU usage calculation to use integer value

--- info.sh
+++ info.sh
@@ -7,7 +7,7 @@ echo
 e=$(who | wc -l)
 echo "nbe d'users co : $e"
 echo
-a=$(top -bn1 | grep "Cpu(s)" | grep -o '[0-9.]\+ id' | cut -d' ' -f1 | awk '{print ($1+0.5)}')
+a=$(top -bn1 | grep "Cpu(s)" | grep -o '[0-9.]\+ id' | cut -d' ' -f1 | awk '{print int($1+0.5)}')
 echo "usage CPU (%)  : $a"
 echo
 b=$(free -m | grep "Mem" | awk '{printf "%.0f\n", $3*100/$2 }')
