a1d7f4a449812ce175b9fa4eb490a1bbc2660292
gymhub101 <johnbagger288@gmail.com>
2026-03-26 08:56:12 +0000

    update info_sh.txt and crise.sh

--- crise.sh
+++ crise.sh
@@ -1,53 +1,46 @@
 #!/bin/bash
 
-max_proc=$(grep proc criteres.txt | cut -d':' -f2)
-echo "$max_proc"
-max_user=$(grep user criteres.txt | cut -d':' -f2)
-echo "$max_user"
-max_cpu=$(grep cpu criteres.txt | cut -d':' -f2)
-echo "$max_cpu"
-max_ram=$(grep  ram criteres.txt | cut -d':' -f2)
-echo "$max_ram"
-max_disk=$(grep proc criteres.txt | cut -d':' -f2)
-echo "$max_disk"
-max_histo=$(grep hist criteres.txt | cut -d':' -f2)
-echo "$max_histo"
+max_proc=$(grep proc criteres.txt | cut -d':' -f2 | tr -d ' ')
 
-proce=$(grep proc info_sh.txt | tail -n1 | cut -d':' -f2 | sed "s/ //g")
-users=$(grep user info_sh.txt | tail -n1 | cut -d':' -f2 | sed "s/ //g")
-cpu=$(grep CPU info_sh.txt | tail -n1 | cut -d':' -f2 | sed "s/ //g")
-ram=$(grep RAM info_sh.txt | tail -n1 | cut -d':' -f2 | sed "s/ //g")
-disk=$(grep disk info_sh.txt | tail -n1 | cut -d':' -f2 | sed "s/ //g")
+max_user=$(grep user criteres.txt | cut -d':' -f2 | tr -d ' ')
 
-mail_label=()
-mail_val=()
+max_cpu=$(grep cpu criteres.txt | cut -d':' -f2 | tr -d ' ')
 
-if ["$proce" -gt "$max_proce"]; then
-mail_label=('processus')
-mail_val=("$proce")
+max_ram=$(grep  ram criteres.txt | cut -d':' -f2 | tr -d ' ')
 
+max_disk=$(grep proc criteres.txt | cut -d':' -f2 | tr -d ' ')
+
+max_histo=$(grep hist criteres.txt | cut -d':' -f2 | tr -d ' ')
+echo "$max_proc $max_user $max_cpu $max_ram $max_disk $max_histo"
+
+proce=$(grep proc info_sh.txt | tail -n1 | cut -d':' -f2 | tr -d ' ')
+users=$(grep user info_sh.txt | tail -n1 | cut -d':' -f2 | tr -d ' ')
+cpu=$(grep CPU info_sh.txt | tail -n1 | cut -d':' -f2 | tr -d ' ')
+ram=$(grep RAM info_sh.txt | tail -n1 | cut -d':' -f2 | tr -d ' ')
+disk=$(grep disk info_sh.txt | tail -n1 | cut -d':' -f2 | tr -d ' ')
+echo "$proce $users $cpu $ram $disk"
+#CANT COMPARE FLOAT AND INT THATS WHY ERROR
+
+info="voici les caracteristiques critiques : "
+
+if [ "$proce" -gt "$max_proce" ]; then
+info="$info processus ($proce),"
 fi
 
-if ["$users" -gt "$max_user"]; then
-mail_label+=('utilisateurs')
-mail_val+=("$users")
-
+if [ "$users" -gt "$max_user" ]; then
+info="$info users ($users),"
 fi
 
-if["$cpu" -gt "$max_cpu"]; then
-mail_label+=('cpu')
-mail_val+=("$cpu")
-
+if[ "$cpu" -gt "$max_cpu" ]; then
+info="$info cpu ($cpu),"
 fi
 
-if ["$ram" -gt "$max_ram"]; then
-mail_label+=('ram')
-mail_val+=("$ram")
-
+if [ "$ram" -gt "$max_ram" ]; then
+info="$info ram ($ram),"
 fi
 
-if ["$disk" -gt "$max_disk"]; then
-mail_label+=('disque')
-mail_val+=("$disk")
-
+if [ "$disk" -gt "$max_disk" ]; then
+info="$info disk ($disk),"
 fi
+
+echo "$info"
