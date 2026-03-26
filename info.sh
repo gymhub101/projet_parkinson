284d3d92e2e24c0b108795ddcd8d1e29d63c0361
gymhub101 <johnbagger288@gmail.com>
2026-03-26 22:27:17 +0100

    Update info.sh

--- info.sh
+++ info.sh
@@ -7,12 +7,12 @@ echo
 e=$(who | wc -l)
 echo "nbe d'users co : $e"
 echo
-a=$(top -bn1 | awk '/Cpu/ { print $7}' | cut -d"," -f2 | bc -l)
+a=$(top -bn1 | grep "Cpu(s)" | grep -o '[0-9.]\+ id' | cut -d' ' -f1 | awk '{print ($1+0.5)}')
 echo "usage CPU (%)  : $a"
 echo
 b=$(free -m | grep "Mem" | awk '{printf "%.0f\n", $3*100/$2 }')
 echo "usage RAM (%)  : $b"
 echo
-d=$(df -h / | grep "dev" | awk '{print $3*100/$2 }' | bc -l)
+d=$(df -h / | grep "dev" | awk '{printf "%.0f\n", $3*100/$2 }')
 echo "usage disk (%) : $d"
 echo
