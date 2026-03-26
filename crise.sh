01b5ccfa6a4b7cb5eb9d15dc1adf2012e849e8c1
gymhub101 <johnbagger288@gmail.com>
2026-03-26 18:07:15 +0100

    Update crise.sh

--- crise.sh
+++ crise.sh
@@ -8,7 +8,7 @@ max_cpu=$(grep cpu criteres.txt | cut -d':' -f2 | tr -d ' ')
 
 max_ram=$(grep  ram criteres.txt | cut -d':' -f2 | tr -d ' ')
 
-max_disk=$(grep proc criteres.txt | cut -d':' -f2 | tr -d ' ')
+max_disk=$(grep disk criteres.txt | cut -d':' -f2 | tr -d ' ')
 
 max_histo=$(grep hist criteres.txt | cut -d':' -f2 | tr -d ' ')
 echo "$max_proc $max_user $max_cpu $max_ram $max_disk $max_histo"
@@ -22,7 +22,7 @@ echo "$proce $users $cpu $ram $disk"
 #CANT COMPARE FLOAT AND INT THATS WHY ERROR
 
 info="voici les caracteristiques critiques : "
-
+# if (( $(echo "$proce > $max_proce" | bc -l) )); then
 if [ "$proce" -gt "$max_proce" ]; then
 info="$info processus ($proce),"
 fi
