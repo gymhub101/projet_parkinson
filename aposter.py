9860bb7d9b1c471692d2b854832efd1f5c16124b
gymhub101 <johnbagger288@gmail.com>
2026-04-06 13:17:06 +0200

    a
    a

--- aposter.py
+++ aposter.py
@@ -5,24 +5,24 @@ from email.mime.base import MIMEBase
 from email import encoders
 import cairosvg  
 
-sender = "Johnbagger288@gmail.com"
-receiver = "Johnbagger288@gmail.com"
-password = "axpunwmvzdwzdysf"
+envoyer = "Johnbagger288@gmail.com"
+recu = "Johnbagger288@gmail.com"
+mdp_appli = "axpunwmvzdwzdysf"
 
-svg_file = "machine.svg"
-png_file = "machine.png"
+svg = "machine.svg"
+png = "machine.png"
 
-cairosvg.svg2png(url=svg_file, write_to=png_file)
+cairosvg.svg2png(url=svg, write_to=png)
 
 msg = MIMEMultipart()
-msg['From'] = sender
-msg['To'] = receiver
+msg['From'] = envoyer
+msg['To'] = recu
 msg['Subject'] = "alerte fatale parc informatique situation de crise !!!"
 
-body = "Le graphe affichant les données de machine est joint ci-dessous."
-msg.attach(MIMEText(body, 'plain'))
+info = "Le graphe affichant les données de machine est joint ci-dessous."
+msg.attach(MIMEText(info, 'plain'))
 
-with open(png_file, "rb") as f:
+with open(png, "rb") as f:
     part = MIMEBase('image', 'png')
     part.set_payload(f.read())
     encoders.encode_base64(part)
@@ -30,7 +30,5 @@ with open(png_file, "rb") as f:
     msg.attach(part)
 
 with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-    server.login(sender, password)
+    server.login(envoyer, mdp_appli)
     server.send_message(msg)
-
-print("Email with PNG attached successfully!")
