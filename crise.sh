489d02437c1320ac05ab819e27f9d43b75d0bb11
gymhub101 <johnbagger288@gmail.com>
2026-04-06 09:41:52 +0200

    a
    a

--- crise.sh
+++ crise.sh
@@ -11,15 +11,34 @@ max_ram=$(grep  ram criteres.txt | cut -d':' -f2 | tr -d ' ')
 max_disk=$(grep disk criteres.txt | cut -d':' -f2 | tr -d ' ')
 
 max_histo=$(grep hist criteres.txt | cut -d':' -f2 | tr -d ' ')
-echo "$max_proc $max_user $max_cpu $max_ram $max_disk $max_histo"
 
 proce=$(grep proc info_sh.txt | tail -n1 | cut -d':' -f2 | tr -d ' ')
 users=$(grep user info_sh.txt | tail -n1 | cut -d':' -f2 | tr -d ' ')
 cpu=$(grep CPU info_sh.txt | tail -n1 | cut -d':' -f2 | tr -d ' ')
 ram=$(grep RAM info_sh.txt | tail -n1 | cut -d':' -f2 | tr -d ' ')
 disk=$(grep disk info_sh.txt | tail -n1 | cut -d':' -f2 | tr -d ' ')
-echo "$proce $users $cpu $ram $disk"
-#CANT COMPARE FLOAT AND INT THATS WHY ERROR
+
+proce1="[$(grep proc info_sh.txt | tail -n "$max_histo" | cut -d':' -f2 | tr -d ' ' | tr '\n' ',' | sed 's/,$//')]"
+users1="[$(grep user info_sh.txt | tail -n "$max_histo"  | cut -d':' -f2 | tr -d ' ' | tr '\n' ',' | sed 's/,$//')]"
+cpu1="[$(grep CPU info_sh.txt | tail -n "$max_histo"  | cut -d':' -f2 | tr -d ' ' | tr '\n' ',' | sed 's/,$//')]"
+ram1="[$(grep RAM info_sh.txt | tail -n "$max_histo"  | cut -d':' -f2 | tr -d ' ' | tr '\n' ',' | sed 's/,$//')]"
+disk1="[$(grep disk info_sh.txt | tail -n "$max_histo"  | cut -d':' -f2 | tr -d ' ' | tr '\n' ',' | sed 's/,$//')]"
+
+date1="[$(grep 2026 info_sh.txt | tail -n "$max_histo" | sed -E '
+s/^[a-zéû\.]+ //;
+s/janvier/01/; s/février/02/;              
+s/mars/03/; s/avril/04/; s/mai/05/;
+s/juin/06/; s/juillet/07/; s/août/08/;
+s/septembre/09/; s/octobre/10/; s/novembre/11/; s/décembre/12/;
+s/ UTC$//                            
+' | awk '{print $1"/"$2"/"$3" "$4}' | tr '\n' ',' | sed 's/,$//')]"
+
+echo $proce1 > data.txt
+echo $users1 >> data.txt
+echo $cpu1 >> data.txt
+echo $ram1 >> data.txt
+echo $disk1 >> data.txt
+echo $date1 >> data.txt
 
 info="voici les caracteristiques critiques : "
 mailing=false
@@ -35,22 +54,23 @@ mailing=true
 fi
 
 if [ "$cpu" -gt "$max_cpu" ]; then
-info="$info cpu ($cpu, limite : $max_cpu)"
+info="$info cpu ($cpu, limite % : $max_cpu)"
 mailing=true
 fi
 
 if [ "$ram" -gt "$max_ram" ]; then
-info="$info ram ($ram, limite : $max_ram)"
+info="$info ram ($ram, limite % : $max_ram)"
 mailing=true
 fi
 
 if [ "$disk" -gt "$max_disk" ]; then
-info="$info disk ($disk, limite : $max_disk)"
+info="$info disk ($disk, limite % : $max_disk)"
 mailing=true
 fi
 
-if [ "$mailing" = "true" ]; then
-echo "$info"
-mail -s "SITUATION CRITIQUE DETECTEE DANS LE PARC INFORMATIQUE" abdoul-malik.aboubakar@univ-avignon.fr  < /etc/fstab
 
+if [ "$mailing" = "true" ]; then
+echo "$info" > aposter.txt
+else
+echo > aposter.txt
 fi
